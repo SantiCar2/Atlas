b3d6f811f30146a815e29144d02e1219ea0aa2de
Santiago Cardona <santiago.cardona56@eia.edu.co>
2024-04-22 20:34:54 -0500

    First commit

---
+++ app.ts
@@ -0,0 +1,44 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+import * as url from 'url';
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      //preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.setMenu(null);
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'dist/atlas/browser/index.html'),
+      protocol: 'file:',
+      slashes: true,
+    }),
+  );
+
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', () => {
+    app.quit();
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
