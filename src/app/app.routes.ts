a50ff30c71171bf82064d7f57860c562515ca720
Santiago Cardona <santiago.cardona56@eia.edu.co>
2024-05-11 15:24:05 -0500

    refactor: Update routes and components for home, library, and downloads pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
 import { SettingsComponent } from './pages/settings/settings.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LibraryComponent } from './pages/library/library.component';
+import { DownloadsComponent } from './pages/downloads/downloads.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HeaderComponent },
-  { path: 'library', component: HeaderComponent },
-  { path: 'downloads', component: HeaderComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'library', component: LibraryComponent },
+  { path: 'downloads', component: DownloadsComponent },
   { path: 'settings', component: SettingsComponent },
 ];
